f1a1224f034318aa23149fbb03a0968f99040dc6
urraccon <133591634+urraccon@users.noreply.github.com>
2023-12-12 13:32:38 +0000

    Deploying to gh-pages from @ urraccon/goit-js-hw-09@52235758b64b536a884828698389bf66e2ba3ae0 🚀

---
+++ 01-color-switcher.f3d051e3.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),o=document.querySelector("body");let d="";function n(){o.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}console.dir(e),console.dir(t),console.dir(o),e.addEventListener("click",(function(){d=setInterval(n,1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){clearInterval(d),t.disabled=!0,e.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.f3d051e3.js.map
