18694d55579f2f995cf759ca6de99ccde326f503
urraccon <alexandru_marian1998@yahoo.com>
2023-12-15 15:36:24 +0200

    finished exercise 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,42 @@
+const formContainer = document.querySelector('.form');
+let numbOfPromise = 0;
+let waitingTime = 0;
+
+formContainer.addEventListener('submit', submittedForm => {
+  submittedForm.preventDefault();
+  console.dir(submittedForm);
+  const typedStartWaitingTime = submittedForm.target[0].valueAsNumber;
+  const typedWaitingTime = submittedForm.target[1].valueAsNumber;
+  const typedTimes = submittedForm.target[2].valueAsNumber;
+  console.log(typedStartWaitingTime, typedWaitingTime, typedTimes);
+  for (let i = 1; i <= typedTimes; i++) {
+    // debugger;
+    numbOfPromise = i;
+    waitingTime = waitingTime + typedWaitingTime;
+    if (numbOfPromise === 1) {
+      waitingTime = typedStartWaitingTime;
+    }
+    new Promise(resolve => {
+      resolve(createPromise(numbOfPromise, waitingTime));
+    })
+      .then(() => {
+        console.log('The promise was made');
+      })
+      .catch(() => {
+        console.log("The promise couldn't be made");
+      });
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill
+    setTimeout(() => {
+      window.alert(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }, delay);
   } else {
-    // Reject
+    setTimeout(() => {
+      window.alert(`❌ Rejected promise ${position} in ${delay}ms`);
+    }, delay);
   }
 }
